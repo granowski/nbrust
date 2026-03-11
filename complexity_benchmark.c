e10debd535bd2fa9f555df7bebe8e209751e1fcc
Derrick Granowski <granowski@coffeetariat.net>
2026-03-11 17:13:10 -0400

    Remove test programs and output the ai run generated

--- complexity_benchmark.c
+++
@@ -1,1 +0,0 @@
-Unexpected token ( (type 5), expected type 3 at line 8
--- hello.c
+++
@@ -1,8 +0,0 @@
-// Target: x86_64-unknown-netbsd
-#include <stdio.h>
-
-int main() {
-    const char* name = "Rust";
-printf("Hello, {}!", name); printf("\n");
-printf("The value is: {}", 42); printf("\n");
-}
